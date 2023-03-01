8350158ed3443dbbce745f745b8bf10cc16a055a
miguelpb11 <maparada@unimonserrate.edu.co>
2023-03-01 07:17:06 -0500

    cambios 01/03/23

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # proyecto1
+
+el método wait_key permite pausar y reanudar cada contador por separado. El usuario puede ingresar el número "1" para pausar contador 1 y el número "2" para pausar el contador 2. El usuario también puede ingresar los números "11" y "22" para reanudar cada contador por separado.
+
+El método pause simplemente establece la variable paused en True, mientras que el método resume establece la variable paused en False y notifica al hilo en espera para que reanude la ejecución.
+
+se crean dos instancias de la clase Contador y se llama al método start en cada una de ellas. El programa esperará a que el usuario ingrese los números "1" y "2" para pausar y los numeros "11" y "22" para reanudar cada contador por separado.
+
+La clase contador cuenta con dos argumentos id y posicion (para especificar la posicion en la pantalla donde se imprimira el contador)
+
+El método run imprime el contador en la posición correspondiente utilizando los caracteres de control de terminal \033[{position}G que establece la posición del cursor en la columna posicion. El primer contador se muestra en la columna 1 y el segundo contador se muestra en la columna 20.
+
+Este código es útil para aplicaciones que necesitan realizar varias tareas simultáneamente y que deben pausarse y reanudarse por separado en respuesta a eventos específicos.
--- proyecto1.py
+++ proyecto1.py
@@ -1,1 +1,170 @@
-print("hola mundo")
+
+"""import timeo
+import multiprocessing
+
+# Función que se ejecuta en el proceso
+def proceso(event):
+    while True:
+        if event.is_set():  # Si el evento está configurado, el proceso está en pausa
+            print("Proceso pausado")
+            time.sleep(1)
+        else:  # Si el evento no está configurado, el proceso está en ejecución
+            print("Proceso en ejecución")
+            time.sleep(1)
+
+if __name__ == '__main__':
+    # Crear un objeto Event para indicar cuándo el proceso debe pausarse
+    pausa_event = multiprocessing.Event()
+    
+    # Crear un objeto de proceso y pasarle el objeto Event
+    p = multiprocessing.Process(target=proceso, args=(pausa_event,))
+    
+    # Iniciar el proceso
+    p.start()
+    
+    # Esperar 5 segundos
+    time.sleep(5)
+    
+    # Pausar el proceso
+    print("Pausando el proceso...")
+    pausa_event.set()
+    
+    # Esperar 5 segundos
+    time.sleep(5)
+    
+    # Reanudar el proceso
+    print("Reanudando el proceso...")
+    pausa_event.clear()
+    
+    # Esperar a que el proceso termine
+    p.join()"""
+
+"""import time
+
+import threading
+
+class Contador:
+    def __init__(self, id):
+        self.id = id
+        self.count = 0
+        self.paused = False
+        self.pause_cond = threading.Condition(threading.Lock())
+    
+    def start(self):
+        self.thread = threading.Thread(target=self.run)
+        self.thread.start()
+    
+    def run(self):
+        while True:
+            with self.pause_cond:
+                while self.paused:
+                    self.pause_cond.wait()
+                self.count += 1
+                print(f"Contador {self.id}: {self.count}")
+            time.sleep(1)
+    
+    def pause(self):
+        self.paused = True
+    
+    def resume(self):
+        with self.pause_cond:
+            self.paused = False
+            self.pause_cond.notify()
+    
+    def wait_key(self):
+        while True:
+            key = input()
+            if key == '1':
+                self.paused = not self.paused
+            elif key == '2':
+                self.paused = not self.paused
+
+# Crea dos contadores
+contador1 = Contador(1)
+contador2 = Contador(2)
+
+# Inicia los contadores en hilos separados
+contador1.start()
+contador2.start()
+
+# Espera a que el usuario ingrese los números 1 y 2
+while True:
+    key = input()
+    if key == '1':
+        contador1.pause()
+    elif key == '2':
+        contador2.pause()
+    elif key == '11':
+        contador1.resume()
+    elif key == '22':
+        contador2.resume()"""
+
+import time
+import threading
+
+class Contador:
+    def __init__(self, id, posicion):
+        self.id = id
+        self.count = 0
+        self.paused = False
+        self.pause_cond = threading.Condition(threading.Lock())
+        self.posicion = posicion
+
+
+        
+    
+    def start(self):
+        self.thread = threading.Thread(target=self.run)
+        self.thread.start()
+
+    
+    def run(self):
+        while True:
+            with self.pause_cond:
+                while self.paused:
+                    self.pause_cond.wait()
+                self.count += 1
+                print(f"\033[{self.posicion}GContador {self.id}: {self.count}")
+                time.sleep(2)
+
+    
+    def pause(self):
+        self.paused = True
+    
+    def resume(self):
+        with self.pause_cond:
+            self.paused = False
+            self.pause_cond.notify()
+
+            
+    
+    def wait_key(self):
+        while True:
+            key = input()
+            if key == '1':
+                self.paused = not self.paused
+            elif key == '2':
+                self.paused = not self.paused
+
+
+# Crea dos contadores
+contador1 = Contador(1, 1)
+contador2 = Contador(2, 20)
+
+# Inicia los contadores en hilos separados
+contador1.start()
+contador2.start()
+
+# Espera a que el usuario ingrese los números 1 y 2
+while True:
+    key = input()
+    if key == '1':
+        contador1.pause()
+    elif key == '2':
+        contador2.pause()
+    elif key == '11':
+        contador1.resume()
+    elif key == '22':
+        contador2.resume()
+
+
